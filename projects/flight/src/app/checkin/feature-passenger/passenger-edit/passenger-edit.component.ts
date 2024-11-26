d16a48543eecf19559b3079a068b8b75fc325728
mikezks <23002661+mikezks@users.noreply.github.com>
2024-11-26 13:52:47 +0100

    refactor: rxjs interop

--- projects/flight/src/app/checkin/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ projects/flight/src/app/checkin/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,12 +1,13 @@
 import { initialPassenger, Passenger } from './../../logic-passenger/model/passenger';
 import { NgIf } from '@angular/common';
-import { Component, effect, inject, input, numberAttribute, signal } from '@angular/core';
+import { Component, effect, inject, Injector, input, numberAttribute, signal } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { validatePassengerStatus } from '../../util-validation';
 import { PassengerService } from '../../logic-passenger/data-access/passenger.service';
 import { switchMap } from 'rxjs';
 import { RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -21,6 +22,8 @@ import { RouterLink } from '@angular/router';
 })
 export class PassengerEditComponent {
   private passengerService = inject(PassengerService);
+  private http = inject(HttpClient);
+  private injector = inject(Injector);
 
   readonly id = input.required<number, string>({ transform: numberAttribute });
 
@@ -34,13 +37,11 @@ export class PassengerEditComponent {
     ]]
   });
 
-  readonly id$ = toObservable(this.id);
-  readonly passenger$ = this.id$.pipe(
-    switchMap(id => this.passengerService.findById(id))
+  readonly passenger = toSignal(
+    toObservable(this.id).pipe(
+      switchMap(id => this.passengerService.findById(id))
+    ), { initialValue: initialPassenger }
   );
-  readonly passenger = toSignal(this.passenger$, {
-    initialValue: initialPassenger
-  });
 
   constructor() {
     effect(() => this.editForm.patchValue(this.passenger()));
@@ -48,5 +49,14 @@ export class PassengerEditComponent {
 
   protected save(): void {
     console.log(this.editForm.value);
+
+    /* const passengerCall = toSignal(this.http.get<Passenger[]>('https://demo.angulararchitects.io/api/passenger?name=Smith'), {
+      initialValue: [],
+      injector: this.injector
+    });
+    effect(() => console.log(passengerCall()), {
+      injector: this.injector,
+      allowSignalWrites: true
+    }); */
   }
 }
