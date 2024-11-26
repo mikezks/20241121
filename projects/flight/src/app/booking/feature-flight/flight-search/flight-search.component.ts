00320602810eeae5cd9d4968315bd2c0a1449975
mikezks <23002661+mikezks@users.noreply.github.com>
2024-11-26 16:41:33 +0100

    refactor: lean component, state mgmt refactored to distinct store

--- projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, effect, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BookingStore, Flight, FlightFilter } from '../../logic-flight';
+import { BookingStore, Flight } from '../../logic-flight';
 import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
 
 
@@ -17,47 +17,13 @@ import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
   templateUrl: './flight-search.component.html',
 })
 export class FlightSearchComponent {
-  private store = inject(BookingStore);
-
-  protected filter = this.store.filter;
-  protected route = computed(
-    () => 'From ' + this.filter().from + ' to ' + this.filter().to + '.'
-  );
-  protected basket = this.store.basket;
-  protected flightResult = this.store.flights;
-
-  constructor() {
-    effect(() => console.log(this.route()));
-  }
-
-  protected search(filter: FlightFilter): void {
-    this.store.setFilter(filter);
-    this.store.loadFlights(filter);
-  }
+  protected store = inject(BookingStore);
 
   protected delay(flight: Flight): void {
-    const oldFlight = flight;
-    const oldDate = new Date(oldFlight.date);
-
-    const newDate = new Date(oldDate.getTime() + 1000 * 60 * 5); // Add 5 min
-    const newFlight = {
-      ...oldFlight,
-      date: newDate.toISOString(),
+    this.store.updateFlights({
+      ...flight,
+      date: new Date(new Date(flight.date).getTime() + 1000 * 60 * 5).toISOString(),
       delayed: true
-    };
-
-    this.store.setFlights(
-      this.flightResult().map(
-        flight => flight.id === newFlight.id ? newFlight : flight
-      )
-    );
-  }
-
-  protected updateBasket(id: number, selected: boolean): void {
-    this.store.setBasketId(id, selected);
-  }
-
-  protected reset(): void {
-    this.store.resetFlights();
+    });
   }
 }
--- projects/flight/src/app/booking/logic-flight/+state/booking.store.ts
+++ projects/flight/src/app/booking/logic-flight/+state/booking.store.ts
@@ -1,8 +1,11 @@
-import { patchState, signalStore, withComputed, withMethods, withState } from "@ngrx/signals";
+import { patchState, signalStore, withComputed, withHooks, withMethods, withState } from "@ngrx/signals";
+import { rxMethod } from "@ngrx/signals/rxjs-interop";
+import { tapResponse } from "@ngrx/operators";
 import { FlightFilter } from "../model/flight-filter";
 import { Flight } from "../model/flight";
 import { computed, inject } from "@angular/core";
 import { FlightService } from "../data-access/flight.service";
+import { pipe, switchMap } from "rxjs";
 
 type BookingState = {
   filter: FlightFilter;
@@ -39,6 +42,9 @@ export const BookingStore = signalStore(
     delayedFlights: computed(
       () => store.flights().filter(flight => flight.delayed)
     ),
+    route: computed(
+      () => 'From ' + store.filter().from + ' to ' + store.filter().to + '.'
+    ),
   })),
   /**
    * Updater
@@ -46,6 +52,11 @@ export const BookingStore = signalStore(
   withMethods(store => ({
     setFilter: (filter: FlightFilter) => patchState(store, { filter }),
     setFlights: (flights: Flight[]) => patchState(store, { flights }),
+    updateFlights: (updatedFlight: Flight) => patchState(store, state => ({
+      flights: state.flights.map(
+        flight => flight.id === updatedFlight.id ? updatedFlight : flight
+      )
+    })),
     setBasketId: (id: number, selected: boolean) => patchState(store, state => ({
       basket: {
         ...state.basket,
@@ -68,6 +79,18 @@ export const BookingStore = signalStore(
 
       flightService.find(filter.from, filter.to, filter.urgent)
         .subscribe(flights => store.setFlights(flights));
-    }
+    },
+    loadRxFlights: rxMethod<FlightFilter>(pipe(
+      switchMap(filter => flightService.find(
+        filter.from, filter.to, filter.urgent
+      )),
+      tapResponse(
+        flights => store.setFlights(flights),
+        err => console.error(err)
+      )
+    ))
   })),
+  withHooks({
+    onInit: store => store.loadRxFlights(store.filter)
+  })
 );
